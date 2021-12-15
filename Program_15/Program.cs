b95e57785fb4f3674d8973aad8c108bdce9e9bed
Илья Филиппов <filippov.i.s@gmail.com>
2021-12-15 23:19:40 +0300

    Добавьте файлы проекта.

---
+++ Program_15/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первый член прогрессии: ");
+            int first = int.Parse(Console.ReadLine());
+            Console.Write("Введите шаг прогрессии: ");
+            int step = int.Parse(Console.ReadLine());
+            Console.Write("Введите количество шагов прогрессии: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Результат арифметической прогресcии");
+            ArithProgression arithProgression = new ArithProgression(step, number);
+            arithProgression.SetStart(first);
+
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine(arithProgression.GetNext());
+                arithProgression.Reset();
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Результат геометрической прогреcсии");
+            GeomProgression geomProgression = new GeomProgression(step, number);
+            geomProgression.SetStart(first);
+
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine(geomProgression.GetNext());
+                geomProgression.Reset();
+            }
+
+            Console.WriteLine();
+            Console.Write("Нажмите на любого клавиша . . .");
+            Console.ReadKey();
+
+        }
+    }
+    interface ISeries
+    {
+        void SetStart(int x);
+        int GetNext();
+        void Reset();
+    }
+
+    class GeomProgression : ISeries
+    {
+
+        int N = 1;
+        int B1;
+        public int B { get; set; }
+        public int Step { get; set; }
+        public int Number { get; set; }
+
+
+        public GeomProgression(int step, int number)
+        {
+            Step = step;
+            Number = number;
+        }
+        public void SetStart(int x)
+        {
+            B1 = B = x;
+        }
+        public int GetNext()
+        {
+            B = (int)(B * Math.Pow(Step, (N - 1)));
+            N++;
+            return (int)B;
+        }
+        public void Reset()
+        {
+            B = B1;
+        }
+    }
+    class ArithProgression : ISeries
+    {
+
+        int N = 1;
+        int A1;
+        public int A { get; set; }
+        public int Step { get; set; }
+        public int Number { get; set; }
+
+
+        public ArithProgression (int step, int number)
+        {
+            Step = step;
+            Number = number;
+        }
+        public void SetStart(int x)
+        {
+            A1 = A = x;
+        }
+        public int GetNext()
+        {
+            A += (N - 1) * Step;
+            N++;
+            return A;
+        }
+        public void Reset()
+        {
+            A = A1;
+        }
+    }
+}
+
+//Разработать интерфейс ISeries генерации ряда чисел. Интерфейс содержит следующие элементы:
+
+//метод void setStart(int x) -устанавливает начальное значение
+//метод int getNext() -возвращает следующее число ряда
+//метод void reset() -выполняет сброс к начальному значению
+//Разработать классы ArithProgression и GeomProgression, которые реализуют интерфейс ISeries.
+//В классах реализовать методы интерфейса в соответствии с логикой арифметической и геометрической прогрессии соответственно.
---
+++ Program_15/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Program_15")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Program_15")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("08ed1153-ea3b-4fc9-8ac9-9c9a2bba937c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
